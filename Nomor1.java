3c8ebefad62d807d24344fb9ce1e0197bfe46c91
AghnaKhaira <aghna.aka@gmail.com>
2024-10-24 08:39:33 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,28 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
-    }
+public class Nomor1{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Total jam lembur: ");
+        int jamLembur = scanner.nextInt();
+        double honorLembur;
+
+        if (jamLembur <= 16) {
+            honorLembur = jamLembur * 10000;
+        } else {
+            honorLembur = (16 * 10000) + ((jamLembur - 16) * 15000);
+        }
+
+        System.out.println("Honor lembur: " + honorLembur);;
 }
+}
+
+
+ALGORITMA
+INPUT jumlah jam libur
+Jika jam lembur <= 16:
+Honor lembur = jam lembur * 10.000
+Jika jam lembur > 16:
+Honor lembur = (16 * 10.000) + ((jam lembur - 16) * 15.000)
+Tampilkan total jam lembur dan honor lembur
